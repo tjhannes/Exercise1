56857f47f535ddeb930e6acc58d1cb780afc70b5
tjhannes <johannes_thanner@live.de>
2017-11-06 17:22:27 +0100

    HelloUser added

---
+++ src/HelloUser.java
@@ -0,0 +1,8 @@
+
+public class HelloUser {
+
+	
+	public static void greetUser(String name){
+		System.out.println("Hello "+name+"- How are you? :)");
+	}
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 
 public class HelloWorld {
+	
+	static String name;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		name = readString("Name: ");
+		HelloUser.greetUser(name);
 	}
+	
+	public static String readString(String text) {
+	    JFrame frame = new JFrame();
+	    String s = JOptionPane.showInputDialog(frame, text);
+	    frame.dispose();
+
+	    if (s == null)
+	      System.exit(0);
+	    return s;
+	  }
 
 }
