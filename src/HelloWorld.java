f5ada4e7af717693d5eccbf58286ae9650393f78
tjhannes <johannes_thanner@live.de>
2017-11-08 13:14:52 +0100

    teset

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("Hello ASE2017 - How are you?:)");
 	
 		System.out.print("Please enter your name: ");
 		Scanner in = new Scanner(System.in);
