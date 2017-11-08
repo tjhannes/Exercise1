fac3211a4ab251fb6b6ec8ecdd86137d3836b339
tjhannes <johannes_thanner@live.de>
2017-11-08 12:59:34 +0100

    neue Upload

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
 	
-		System.out.print("Please enter your name:");
+		System.out.print("Please enter your name: ");
 		Scanner in = new Scanner(System.in);
 		String name = in.nextLine();  //name = readString("Name: ");
 		HelloUser user1 = new HelloUser(name);
